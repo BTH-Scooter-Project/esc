471b5524946e3c1b022c9236609f734f6cbc4dfc
Nenad Cuturic <cnesko@gmail.com>
2021-12-02 10:47:23 +0100

    Some clean up

--- esc/main.py
+++ esc/main.py
@@ -30,7 +30,6 @@ def main():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # print_hi('PyCharm')
     main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
