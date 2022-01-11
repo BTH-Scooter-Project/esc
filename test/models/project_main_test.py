2cf9b65b4d28e2d819f6b9bbe5cbf76e4ff169f4
Nenad Cuturic <cnesko@gmail.com>
2022-01-12 00:10:05 +0100

    More unittest coverage

--- test/models/project_main_test.py
+++ test/models/project_main_test.py
@@ -45,5 +45,73 @@ class ProjectMainTestCase(flask_unittest.ClientTestCase):
         }
         response = client.post('/login', data=data)
         self.assertEqual(response.status_code, 302, "Redirect to /profile")
+
+        data = {
+            "email": self.config['email'],
+            "password": 'wrong'
+        }
+        response = client.post('/login', data=data)
+        self.assertEqual(response.status_code, 302, "Redirect to /profile")
+
+    def test_app_4_profile_get_and_post(self, client):
+        """Test customer interface login."""
+        data = {
+            "email": self.config['email'],
+            "password": self.config['password']
+        }
+        response = client.post('/login', data=data)
+
+        response = client.get('/profile')
+        self.assertEqual(response.status_code, 200)
+
+        data = {
+            "email": self.config['email'],
+            "password": self.config['password']
+        }
+        response = client.post('/profile')
+        self.assertEqual(response.status_code, 302)
+
+    def test_app_6_travel_get(self, client):
+        """Test customer interface login."""
+        data = {
+            "email": self.config['email'],
+            "password": self.config['password']
+        }
+        response = client.post('/login', data=data)
+        response = client.get('/travels')
+        self.assertEqual(response.status_code, 200)
+        
         # self.assertIn(b'Login', response.data)
         # self.assertIn('Sign in with Google', response.data.decode("utf-8"))
+
+    def test_app_7_signup_get_and_post(self, client):
+        """Test customer interface login."""
+        data = {
+            "email": self.config['email'],
+            "password": self.config['password'],
+            "balance": self.config['balance'],
+            "payment": self.config['payment'],
+            "firstname": self.config['firstname'],
+            "lastname": self.config['lastname']
+        }
+        response = client.get('/signup')
+        self.assertEqual(response.status_code, 200)
+
+        response = client.post('/signup', data=data)
+        self.assertEqual(response.status_code, 302, "Redirect to /login")
+
+    def test_app_8_logout(self, client):
+        """Test customer interface login."""
+        data = {
+            "email": self.config['email'],
+            "password": self.config['password']
+        }
+        response = client.post('/login', data=data)
+
+        response = client.get('/logout')
+        self.assertEqual(response.status_code, 302, "Redirect to /")
+
+    def test_app_9_callback(self, client):
+        """Test customer interface login."""
+        response = client.get('/login/callback')
+        self.assertEqual(response.status_code, 500, "Redirect to /profile")
